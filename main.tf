850c3083c52188ad4dfb5b54ea40a18ba423524f
naveenkondate123 <naveenkondate123@gmail.com>
2026-03-25 16:41:56 +0530

    new

---
+++ main.tf
@@ -0,0 +1,16 @@
+apiVerssion:platform.conflunet.io/v1
+kind: deploymet
+ metadata:
+    replica:4
+        resources:
+         limits:
+          memory:
+          cpu:
+        requests:
+         memory:
+         cpu:
+
+ nodeAffinity:
+  requriedDuringSheduleIgnoreDuringExcution
+  
+        
