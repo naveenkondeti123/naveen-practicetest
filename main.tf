58d7a08582291a2f79a8f91cb7868732e87d0eb2
naveenkondate123 <naveenkondate123@gmail.com>
2026-03-25 16:57:28 +0530

    new

--- main.tf
+++ main.tf
@@ -1,16 +1,38 @@
 apiVerssion:platform.conflunet.io/v1
 kind: deploymet
- metadata:
-    replica:4
+metadata:
+    name: schemaregistrey
+    namespace: eeh-platform-dev
+
+spec
+    replicas:4
         resources:
          limits:
           memory:
           cpu:
-        requests:
-         memory:
-         cpu:
+         requests:
+          memory:
+          cpu:
+        affinity:
+           nodeAffinity:
+             requriedDuringSheduleIgnoreDuringExcution:
+            podAntiAffinity:
+              preferredDuringShedulingIgnoreDuringExcution:
+                podAffinityTerm:
+                  labelselectors:
+                    -key: app
 
- nodeAffinity:
-  requriedDuringSheduleIgnoreDuringExcution
-  
+
+  images:
+   application:
+   pullSecretRef:
+
+dependcies:
+  kafka:
+    bootstrapendpoint:
+    authectication:
+     type:oauth
+
+
+
         
