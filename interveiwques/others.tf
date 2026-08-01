f80d27b9abc8394edfa67bf7df53bfebbe747067
naveenkondeti123 <163740010+naveenkondeti123@users.noreply.github.com>
2026-08-01 09:28:11 +0530

    Update others.tf

--- interveiwques/others.tf
+++ interveiwques/others.tf
@@ -19,13 +19,13 @@ CMD ["node", "dist/index.js"]
 # Build Stage
 FROM maven:3.9-eclipse-temurin-17 AS builder
 WORKDIR /app
-COPY . .
+COPY Pom.xml .
 RUN mvn clean package -DskipTests
 
 # Runtime Stage
 FROM eclipse-temurin:17-jre-alpine
 WORKDIR /app
-COPY --from=builder /app/target/*.jar app.jar
+COPY --from=builder /app/target/*
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","app.jar"]
 ----
